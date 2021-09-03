7fdedddf3ab50d2ee2b00344614d3ffe9027d27c
laimu <53105734+laimu666@users.noreply.github.com>
2021-09-03 18:34:54 +0800

    集成AntDesignVue

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,13 +1,33 @@
 <template>
 <div>这是登录页</div>
 <div>{{name}}</div>
+<HomeOutlined />
+<SettingFilled />
+<SmileOutlined />
+<SyncOutlined spin />
+<SmileOutlined :rotate="180" />
+<LoadingOutlined />
+<a-time-picker v-model:value="value" />
+<a-time-picker v-model:value="strValue" valueFormat="HH:mm:ss" />
 </template>
 
 <script lang='ts' setup>
-import { computed } from 'vue'
+import { defineComponent, ref, watch, computed } from 'vue'
 import { useStore } from '../../store'
-const store = useStore()
-const name = computed(() => store.state.name)
+import moment, { Moment } from 'moment'
+import {
+  HomeOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+  LoadingOutlined,
+  timePicker
+} from '@ant-design/icons-vue'
+
+// const store = useStore()
+// const name = computed(() => store.state.name)
+const value = ref<Moment>(moment('08:00:00', 'HH:mm:ss'))
+const strValue = ref<string>('09:00:00')
 </script>
 
 <style scoped lang='scss'>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteComponents, { AntDesignVueResolver } from 'vite-plugin-components';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    // 按需加载ant-design-vue组件
+    ViteComponents({
+      customComponentResolvers: [AntDesignVueResolver()],
+    }),
+  ]
 })
